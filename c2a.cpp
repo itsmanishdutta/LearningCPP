ff978c27d1605b1033d15858ec5855f33834b54d
Manish Kumar Dutta <itsmanishdutta@gmail.com>
2026-09-04 17:28:44 +0530

    update

---
+++ c2a.cpp
@@ -0,0 +1,12 @@
+
+// This program helps you to convert character to ASCII
+#include <iostream>
+using namespace std;
+int main(){
+    char a;
+    cout<<"Enter a character:";
+    cin>>a;
+    int as= (int) a;
+    cout<<"The value in ASCII of "<<a<<" is "<<as;
+    
+}
