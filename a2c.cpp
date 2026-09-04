c1a2246f9c1ddcb65a5302c1c77d71c9330da01c
Manish Kumar Dutta <itsmanishdutta@gmail.com>
2026-09-04 17:36:22 +0530

    ASCII to Character

---
+++ a2c.cpp
@@ -0,0 +1,10 @@
+// This program converts ASCII to character
+#include <iostream>
+using namespace std;
+int main(){
+    int as;
+    cout<<"Enter the ASCII value: ";
+    cin>>as;
+    char ch = (char) as;
+    cout<< "The ACII value " <<as<<" is of "<<ch;
+}
