e2e623df43a4ad27420641071b66c9b6be4ab413
Manish Kumar Dutta <itsmanishdutta@gmail.com>
2026-09-09 23:19:22 +0530

    This program calculates the sum of even numbers entered by user as well as sum of odd numbers entered by user

---
+++ pro.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+using namespace std;
+void sumEven(){
+    int num;
+    cout<<"How many numbers you want to enter:";
+    cin>>num;
+    int numbers;
+    int even=0;
+    int odd=0;
+    for(int i=1; i<=num;i++){
+        cout<<"Enter the "<<i<<"th position number:";
+        cin>>numbers;
+        if (numbers%2==0){
+            even=even+numbers;
+        }
+        else{
+odd= odd+numbers;
+        }
+    }
+    cout<<"The sum of only even number is: "<< even<<endl;
+    cout<<"The sum of only odd number is: "<< odd;
+
+
+}
+
+int main(){
+    sumEven();
+}
